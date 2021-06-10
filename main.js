6fdf1c889eb537a8263623184ae30948cc7548ae
Zain aftab <zain.aftab@z57.com>
2021-06-10 18:07:41 +0500

    updated cor url

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server,{
   cors: {
-    origin: "http://www.stage02.zengtest6.us/",
+    origin: "http://www.stage02.zengtest6.us",
     methods: ["GET", "POST"]
   }
 });
